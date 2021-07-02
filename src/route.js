42512f7d24f82877934fc21c6cce2e2eef6e304e
Victor Hugo Cordeiro Correa <viktorhugo.dev@gmail.com>
2021-07-02 04:46:50 -0300

    versão 1.0

---
+++ src/route.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
+const route = express.Router();
+
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
+route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
+
+route.post('/question/:room/:question/:action', QuestionController.index)
+route.post('/question/create/:room', QuestionController.create)
+
+
+route.get('/room/:room', RoomController.open)
+route.post('/create-room', RoomController.create)
+route.post('/enter-room', RoomController.enter)
+
+module.exports = route
